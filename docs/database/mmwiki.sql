bb23d6a6e56c137cd544d00f4e8b106a4fad06b0
phachon <phachon@163.com>
2018-06-08 14:35:00 +0800

    update database

--- docs/database/mmwiki.sql
+++ docs/database/mmwiki.sql
@@ -8,7 +8,7 @@
 -- --------------------------------
 DROP TABLE IF EXISTS `mw_user`;
 CREATE TABLE `mw_user` (
-  `user_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `user_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户 id',
   `username` varchar(100) NOT NULL DEFAULT '' COMMENT '用户名',
   `given_name` varchar(50) NOT NULL DEFAULT '' COMMENT '姓名',
   `mobile` char(13) NOT NULL DEFAULT '' COMMENT '手机号',
@@ -32,9 +32,10 @@ CREATE TABLE `mw_user` (
 -- --------------------------------
 DROP TABLE IF EXISTS `mw_space`;
 CREATE TABLE `mw_space` (
-  `space_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '空间表主键id',
-  `category_id` int(10) NOT NULL DEFAULT '0' COMMENT '空间分类id',
+  `space_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '空间 id',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '名称',
   `description` varchar(100) NOT NULL DEFAULT '' COMMENT '描述',
+  `visit_level` enum('private','internal','public') NOT NULL DEFAULT 'internal' COMMENT '访问级别：private,internal,public',
   `is_delete` tinyint(3) NOT NULL DEFAULT '0' COMMENT '是否删除 0 否 1 是',
   `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
   `update_time` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
@@ -43,34 +44,36 @@ CREATE TABLE `mw_space` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='空间表';
 
 -- --------------------------------
--- 分类表
+-- 空间标签表
 -- --------------------------------
-DROP TABLE IF EXISTS `mw_category`;
-CREATE TABLE `mw_category` (
-  `category_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '分类表主键id',
-  `name` varchar(32) NOT NULL DEFAULT '' COMMENT '分类名',
-  `is_delete` tinyint(3) NOT NULL DEFAULT '0' COMMENT '是否删除 0 否 1 是',
-  `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `update_time` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
-  PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='分类表';
+DROP TABLE IF EXISTS `mw_space_tag`;
+CREATE TABLE `mw_space_tag` (
+  `space_tag_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '空间标签关系 id',
+  `space_id` int(11) unsigned NOT NULL  COMMENT '空间 id',
+  `tag` char(10) NOT NULL DEFAULT '' COMMENT '标签名称',
+  `create_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  PRIMARY KEY (`space_tag_id`),
+  KEY (`space_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='空间标签表';
 
 -- --------------------------------
 -- 文档表
 -- --------------------------------
 DROP TABLE IF EXISTS `mw_page`;
 CREATE TABLE `mw_page` (
-  `page_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '文档表主键id',
-  `parent_id` int(10) NOT NULL DEFAULT '0' COMMENT '文档父id',
+  `page_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '文档 id',
+  `parent_id` int(10) NOT NULL DEFAULT '0' COMMENT '文档父 id',
   `space_id` int(10) NOT NULL DEFAULT '0' COMMENT '空间id',
-  `type` tinyint(3) NOT NULL DEFAULT '' COMMENT '文档类型 1 目录 2 页面',
   `title` varchar(150) NOT NULL DEFAULT '' COMMENT '文档标题',
+  `type` tinyint(3) NOT NULL DEFAULT '1' COMMENT '文档类型 1 页面 2 目录',
   `path` varchar(100) NOT NULL DEFAULT '' COMMENT 'markdown 文件路径',
-  `create_user_id` int(10) NOT NULL DEFAULT '0' COMMENT '创建用户id',
-  `edit_user_id` int(10) NOT NULL DEFAULT '0' COMMENT '最后修改用户id',
+  `create_user_id` int(10) NOT NULL DEFAULT '0' COMMENT '创建用户 id',
+  `edit_user_id` int(10) NOT NULL DEFAULT '0' COMMENT '最后修改用户 id',
   `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
   `update_time` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`page_id`)
+  KEY (`parent_id`),
+  KEY (`space_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档表';
 
 -- --------------------------------
@@ -78,11 +81,15 @@ CREATE TABLE `mw_page` (
 -- --------------------------------
 DROP TABLE IF EXISTS `mw_user_space`;
 CREATE TABLE `mw_user_space` (
-  `user_space_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户项目关系表 id',
+  `user_space_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户空间关系 id',
   `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '用户 id',
   `space_id` int(10) NOT NULL DEFAULT '0' COMMENT '项目 id',
+  `privilege` tinyint(3) NOT NULL DEFAULT '0' COMMENT '用户空间访问权限 0 浏览者 1 编辑者 2 管理员',
   `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
-  PRIMARY KEY (`user_space_id`)
+  PRIMARY KEY (`user_space_id`),
+  UNIQUE KEY (`user_id`, `space_id`),
+  KEY (`user_id`),
+  KEY (`space_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户空间关系表';
 
 -- --------------------------------
@@ -90,14 +97,14 @@ CREATE TABLE `mw_user_space` (
 -- --------------------------------
 DROP TABLE IF EXISTS `mw_collection`;
 CREATE TABLE `mw_collection` (
-  `collection_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户收藏关系表 id',
+  `collection_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户收藏关系 id',
   `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '用户id',
-  `username` varchar(100) NOT NULL DEFAULT '' COMMENT '用户名',
   `type` tinyint(3) NOT NULL DEFAULT '1' COMMENT '收藏类型 1 文档 2 空间',
   `resource_id` int(10) NOT NULL DEFAULT '0' COMMENT '收藏资源 id ',
   `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
   `update_time` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`collection_id`)
+  KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户收藏表';
 
 -- --------------------------------
@@ -105,19 +112,35 @@ CREATE TABLE `mw_collection` (
 -- --------------------------------
 DROP TABLE IF EXISTS `mw_follow`;
 CREATE TABLE `mw_follow` (
-  `follow_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '关注表id',
+  `follow_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '关注 id',
   `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '用户id',
   `follow_user_id` int(10) NOT NULL DEFAULT '0' COMMENT '被关注用户 id',
   `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
-  PRIMARY KEY (`follow_id`)
+  PRIMARY KEY (`follow_id`),
+  KEY (`user_id`),
+  KEY (`follow_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户关注表';
 
 -- --------------------------------
--- 操作日志表
+-- 文档日志表
+-- --------------------------------
+DROP TABLE IF EXISTS `mw_log_page`;
+CREATE TABLE `mw_log_page` (
+  `log_page_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '文档日志 id',
+  `page_id` int(10) NOT NULL DEFAULT '0' COMMENT '文档id',
+  `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '用户id',
+  `action` tinyint(3) NOT NULL DEFAULT '1' COMMENT '动作 1 增加 2 修改 3 删除',
+  `comment` varchar(255) NOT NULL DEFAULT '' COMMENT '备注信息',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  PRIMARY KEY (`log_page_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档日志表';
+
+-- --------------------------------
+-- 系统操作日志表
 -- --------------------------------
 DROP TABLE IF EXISTS `mw_log`;
 CREATE TABLE `mw_log` (
-  `log_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '日志id',
+  `log_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '系统操作日志 id',
   `level` tinyint(3) NOT NULL DEFAULT '6' COMMENT '日志级别',
   `controller` char(100) NOT NULL DEFAULT '' COMMENT '控制器',
   `action` char(100) NOT NULL DEFAULT '' COMMENT '动作',
@@ -127,19 +150,18 @@ CREATE TABLE `mw_log` (
   `ip` char(100) NOT NULL DEFAULT '' COMMENT 'ip地址',
   `user_agent` char(200) NOT NULL DEFAULT '' COMMENT '用户代理',
   `referer` char(100) NOT NULL DEFAULT '' COMMENT 'referer',
-  `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '帐号id',
-  `username` char(100) NOT NULL DEFAULT '' COMMENT '帐号名',
+  `user_id` int(10) NOT NULL DEFAULT '0' COMMENT '用户id',
+  `username` char(100) NOT NULL DEFAULT '' COMMENT '用户名',
   `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
   PRIMARY KEY (`log_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='操作日志表';
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统操作日志表';
 
 -- --------------------------------
--- 联系人表
+-- 系统联系人表
 -- --------------------------------
 DROP TABLE IF EXISTS `mw_contact`;
 CREATE TABLE `mw_contact` (
-  `contact_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '联系人表主键ID',
+  `contact_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '联系人 id',
   `name` varchar(50) NOT NULL DEFAULT '' COMMENT '联系人名称',
   `telephone` char(13) NOT NULL DEFAULT '' COMMENT '联系人座机电话',
   `mobile` char(13) NOT NULL DEFAULT '' COMMENT '联系人手机',
