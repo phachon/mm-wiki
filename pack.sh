3fc9a0512a8f525d2a03424548591f5bab5e8657
sampan <sampan@tencent.com>
2019-12-08 19:52:46 +0800

    fix build

--- pack.sh
+++ pack.sh
@@ -9,24 +9,24 @@ rm -rf ${RELEASE}
 mkdir ${RELEASE}
 
 echo 'Start pack windows amd64...'
-GOOS=windows GOARCH=amd64 && ./build.sh
+GOOS=windows GOARCH=amd64 ./build.sh
 tar -czvf "${RELEASE}/mm-wiki-${VER}-windows-amd64.tar.gz" -C release .
 
 echo 'Start pack windows X386...'
-GOOS=windows GOARCH=386 && ./build.sh
+GOOS=windows GOARCH=386 ./build.sh
 tar -czvf "${RELEASE}/mm-wiki-${VER}-windows-386.tar.gz" -C release .
 
 echo 'Start pack linux amd64'
-GOOS=linux GOARCH=amd64 && ./build.sh
+GOOS=linux GOARCH=amd64 ./build.sh
 ./build.sh
 tar -czvf "${RELEASE}/mm-wiki-${VER}-linux-amd64.tar.gz" -C release .
 
 echo 'Start pack linux 386'
-GOOS=linux GOARCH=386 && ./build.sh
+GOOS=linux GOARCH=386 ./build.sh
 tar -czvf "${RELEASE}/mm-wiki-${VER}-linux-386.tar.gz" -C release .
 
 echo 'Start pack mac amd64'
-GOOS=darwin GOARCH=amd64 && ./build.sh
+GOOS=darwin GOARCH=amd64 ./build.sh
 tar -czvf "${RELEASE}/mm-wiki-${VER}-mac-amd64.tar.gz" -C release .
 
 echo 'END'
