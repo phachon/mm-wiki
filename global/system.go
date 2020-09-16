d429c75f3f0fd32057320b2b0b1d3bbe8db43af8
phachon <phachon@163.com>
2020-09-16 14:05:28 +0800

    add v0.2.1

--- global/system.go
+++ global/system.go
@@ -1,6 +1,6 @@
 package global
 
 const (
-	SYSTEM_VERSION   = "v0.2.0"              // system version code
+	SYSTEM_VERSION   = "v0.2.1"              // system version code
 	SYSTEM_COPYRIGHT = "2018 - 2020 phachon" // system copyright
 )
